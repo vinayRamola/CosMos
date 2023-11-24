1ea41889b76bb1137b92dcfe547cd494852eaeb6
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2023-11-25 01:07:53 +0530

    Don't know what I did but I like it

---
+++ README.md
@@ -0,0 +1,48 @@
+![Sharemate Logo](https://res.cloudinary.com/dgoldjr3g/image/upload/v1688866430/NegProjects/favicon_idgipx.jpg)
+
+#### A Social Media App to connect with people
+
+> Share anything and everything!
+
+## How to install and run locally?
+
+```
+$ git clone https://github.com/BhumikaSingh-27/ShareMate.git
+$ cd sharemate
+$ npm install
+$ npm start
+```
+
+## Features -
+
+- User Feed
+- List all user posts
+- Sort by Date & Trending
+- Create a Post
+- Like a post
+- Edit & Delete Post
+- User Profile
+- Add Avatar
+- Add Bio
+- Add Portfolio URL
+- Follow/Unfollow
+- Explore Feed
+- Bookmark Post
+- Authentication
+  - Sign-up
+  - Login
+  - Logout
+- Loading and Alerts
+
+## Built with -
+
+- React JS
+- React Context API + useReducer
+- React Router v6
+- Vanilla CSS
+- Material UI Icons
+- mockBee backend
+- React Toastify
+
+## Video -
+https://www.loom.com/share/13cf6aa5519940a08840f6f6ce954726
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import "./Footer.css";
+
+//condition location
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer-container"> 
+        <div>
+          Made with ❤️ by{" "}
+          <a
+            // href="https://bhumikawebsite.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="not-link"
+          >
+            {" "}
+            Team COSMOS
+          </a>
+        </div>
+        <div className="footer-links">
+          <div>
+            <a
+             className="not-link"
+              href="https://github.com/BhumikaSingh-27"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+          </div>
+          <div>
+            <a
+            className="not-link"
+              href="https://twitter.com/BhumikaSingh27"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TwitterIcon />
+            </a>
+          </div>
+          <div>
+            <a
+             className="not-link"
+              href="https://www.linkedin.com/in/bhumika-singh-594004184/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <LinkedInIcon />
+            </a>
+          </div>
+        </div>
+        <div>&#169; 2023 | ShareMate</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
